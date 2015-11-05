d14c49f78b13273cb0be80908e1da0942e173b3e
Taova Marina <taomarina@yandex.ru>
2015-11-05 17:08:53 +0300

    brach markups test 5.11.2015

---
+++ config.rb
@@ -0,0 +1,17 @@
+require 'compass'
+
+http_path = "/app"
+css_dir = "app/css"
+sass_dir = "app/styles"
+images_dir = "app/img"
+javascripts_dir = "app/js"
+
+# output_style = :expanded or :nested or :compact or :compressed
+
+output_style = :expanded
+
+relative_assets = false
+
+line_comments = true
+
+preferred_syntax = :scss
