f465b3a5df182eb5db2814509cb0eb8b3a7725d5
LrVch <caden45@mail.ru>
2015-11-08 14:56:03 +0600

    подключил второй спинер для теста

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,42 @@
-$(document).ready(function($) {
-	$(function() {
-    $( "#slider-range" ).slider({
-      // range: true,
-      // min: 0,
-      max: 500,
-      value: 300,
-      // slide: function( event, ui ) {
-      //   $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-      // }
+$(document).ready(function ($) {
+    $(function () {
+        $("#slider-range").slider({
+            // range: true,
+            // min: 0,
+            max: 500,
+            value: 300,
+            // slide: function( event, ui ) {
+            //   $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+            // }
+        });
+        /*$(function () {
+            $("#draggable").draggable();
+        });*/
+        // $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+        //   " - $" + $( "#slider-range" ).slider( "values", 1 ) );
     });
-     $(function() {
-      $( "#draggable" ).draggable();
+    $(function () {
+        $("#spinner_0").spinner({
+            spin: function (event, ui) {
+                if (ui.value > 10) {
+                    $(this).spinner("value", -10);
+                    return false;
+                } else if (ui.value < -10) {
+                    $(this).spinner("value", 10);
+                    return false;
+                }
+            }
+        });
+        $("#spinner_1").spinner({
+            spin: function (event, ui) {
+                if (ui.value > 10) {
+                    $(this).spinner("value", -10);
+                    return false;
+                } else if (ui.value < -10) {
+                    $(this).spinner("value", 10);
+                    return false;
+                }
+            }
+        });
     });
-    // $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-    //   " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-  });
-  $(function() {
-    $( "#spinner" ).spinner({
-      spin: function( event, ui ) {
-        if ( ui.value > 10 ) {
-          $( this ).spinner( "value", -10 );
-          return false;
-        } else if ( ui.value < -10 ) {
-          $( this ).spinner( "value", 10 );
-          return false;
-        }
-      }
-    });
-  });
 });
